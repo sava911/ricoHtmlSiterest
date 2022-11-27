3032237528f6c40f7bce0c5227a0d31a676dcef1
sava911 <pesss929@gmail.com>
2022-11-28 02:08:38 +0300

    finish index

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".header__menu-button__wrapper ").on("click", function () {
+    $(".modal-menu").toggle();
+    // $("body").addClass("lock");
+  });
+  // $(".header__menu-button__wrapper ").on("click", function () {
+  //   $(".modal-menu").removeClass("active");
+  //   $("body").removeClass("lock");
+  // });
+})
